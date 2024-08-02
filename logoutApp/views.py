9be27afd0b738c127f0aa55097d3accdae44ac53
Md.Imran Hossain <ih9114101@gmail.com>
2024-08-02 19:09:24 +0600

    FIX: fix bug, now only logged in user can show the logout button.And they can logout

--- logoutApp/views.py
+++ logoutApp/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 # Create your views here.
-
+@login_required
 def logout_user(request):
     logout(request)
     return render(request, 'Index/index.html')
